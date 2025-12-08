f3aa75e8146e45f72ef6f0daf2efeffbcefed1d0
Thomas Fritzsche <tf@noto.de>
2025-12-08 17:16:50 +0100

    firefox windows

--- contest-processor.js
+++ contest-processor.js
@@ -4,23 +4,29 @@ import { Tst } from "./contest.js"
 class ContestWorkletProcessor extends AudioWorkletProcessor {
 
   constructor(options) {
+    //console.log("audio")
     super();
     Tst.processor = this
     this._contest = Tst //new Contest()
     this._block = new Float32Array(128)
+    this._start = false
     this.port.onmessage = (e) => {
       Tst.onmessage(e.data)
+      this._start = true
     };
+   
   }
 
   process(inputs, outputs, parameters) {
     const output = outputs[0]
-
-    if (Tst.running) {
+    const no_of_channel = output.length
+    if(!this._start) return true
+    if (Tst.running  && no_of_channel > 0) {
       this._contest.getBlock(this._block)
-      for (let channel = 0; channel < output.length; ++channel) {
+      for (let channel = 0; channel < no_of_channel; ++channel) {
         const outputChannel = output[channel];
-        for (let i = 0; i < outputChannel.length; ++i) {
+        const buffer_size = outputChannel.length
+        for (let i = 0; i < buffer_size; ++i) {
           outputChannel[i] = this._block[i];
         }
       }
