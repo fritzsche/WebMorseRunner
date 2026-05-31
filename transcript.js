520f1d3ce2537c31262cedae5d25c889647e652c
Thomas Fritzsche <tf@noto.de>
2026-06-01 00:21:57 +0200

    Added transcript box

--- transcript.js
+++ transcript.js
@@ -11,10 +11,15 @@ export class Transcript {
     init() {
         this._startTime = 0
         this._log = []
+        this._element = document.getElementById('transcript_content')
     }
 
     log(msg) {
         console.log(msg)
+        const line = document.createElement('div')
+        line.textContent = msg
+        this._element.appendChild(line)
+        this._element.scrollTop = this._element.scrollHeight
        // this._log.push(msg)
     }
 }
