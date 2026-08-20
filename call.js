2dd7b523c44b30999c3f17f53d208334bca3ab07
Jonathan Adami <pitilezard@gmail.com>
2026-08-21 09:52:56 +1000

    this was not used

--- call.js
+++ call.js
@@ -54,7 +54,7 @@ export class Calls {
                 return l.substring(0, 1) !== "#";
             })
             .map((e) => {
-                e.trim();
+                e = e.trim();
                 // strip tabs and line breaks to ensure
                 // call signs are just call signs
                 e = e.replace(/[\n\r\t]/gm, "");
